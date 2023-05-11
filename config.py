b6c6b19ea9039aff6ba104d11a257b985c3f775a
Marc Parrikal <marcparrikal@fb.com>
2023-05-11 21:08:42 +0800

    fix config dict

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ paths = {'dict_only': './dict/CSW_2019.txt',
          'dict_with_definitions': './dict/CSW_2019_def.txt',
          'url_twl06': 'https://www.wordgamedictionary.com/twl06/download/twl06.txt'}
 
-defaults = {'default_rack' = "S,C,R,A,B,B,L,E",
-            'min_word_length' = 2,
-            'max_word_length'= 7}
+defaults = {'default_rack': "S,C,R,A,B,B,L,E",
+            'min_word_length': 2,
+            'max_word_length': 7}
